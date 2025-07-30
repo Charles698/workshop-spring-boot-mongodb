68be97960b7bbd27477b60f4c813245ec78dd3ca
Charles Felipe <charles.fmsilva@gmail.com>
2025-07-30 14:25:48 -0300

    Consulta simples com query methods

--- src/main/java/com/charlesfelipe/workshopmongo/services/PostService.java
+++ src/main/java/com/charlesfelipe/workshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.charlesfelipe.workshopmongo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +19,9 @@ public class PostService {
 		Post obj = repository.findById(id)
 				.orElseThrow(() -> new NoSuchElementException("Objeto não encontrado!")); 
 		return obj;
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
 	}
 }
